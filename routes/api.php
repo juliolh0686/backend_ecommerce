642ee7a142e667214b81abde0c3799090b279838
JulioDev <juliolh06@gmail.com>
2025-03-27 22:49:52 -0500

    migraciones_relaciones_table

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DocumentoController;
+use App\Http\Controllers\PermisoController;
+use App\Http\Controllers\PersonaController;
+use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,6 +21,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users/{id}', [UserController::class, 'show']);
     Route::put('/users/{id}', [UserController::class, 'update']);
     Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+    //CRUD Persona
+    Route::apiResource('role', RoleController::class);
+    Route::apiResource('persona', PersonaController::class);
+    Route::apiResource('permiso', PermisoController::class);
+    Route::apiResource('documento', DocumentoController::class);
 });
 
 //Auth
